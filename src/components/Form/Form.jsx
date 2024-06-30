da57b2a06c2ebe10acf4dfadc21c853f24149a30
andrewwwmatsko <matskoandrew@gmail.com>
2024-06-30 11:09:51 +0300

    form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,30 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
 
-export const Form = () => {
-  return <h2>Form</h2>;
+export const Form = ({ onSubmit }) => {
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const value = event.target.elements.search.value.trim();
+
+    onSubmit(value);
+
+    event.target.reset();
+  };
+
+  return (
+    <form className={style.form} onSubmit={handleSubmit}>
+      <button className={style.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <input
+        className={style.input}
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+      />
+    </form>
+  );
 };
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,10 +1,38 @@
-// import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from 'apiService/photos';
+import { Text, Form } from 'components';
+import { useEffect, useState } from 'react';
 
 export const Photos = () => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const [photos, setPhotos] = useState([]);
+
+  const [error, setError] = useState('');
+
+  const onSubmitHandler = value => {
+    setSearchValue(value);
+  };
+
+  useEffect(() => {
+    if (!searchValue) return;
+
+    const fetch = async () => {
+      try {
+        const data = await getPhotos(searchValue);
+        setPhotos(data.photos);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetch();
+  }, [searchValue]);
+
   return (
     <>
       <Text textAlign="center">Let`s begin search 🔎</Text>
+
+      <Form onSubmit={onSubmitHandler} />
     </>
   );
 };
