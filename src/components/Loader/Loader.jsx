98576e9bce6f8714750a8aadf4daa4e3f50edc6b
Olha-Rizhko <152223653+Olha-Rizhko@users.noreply.github.com>
2024-06-30 11:05:41 +0200

    photo gallery

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,3 +1,10 @@
+import { BeatLoader } from 'react-spinners';
+import style from './Loader.module.css';
+
 export const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className={style.backdrop}>
+      <BeatLoader />
+    </div>
+  );
 };
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,5 +1,5 @@
 import { getPhotos } from 'apiService/photos';
-import { Text, Form } from 'components';
+import { Text, Form, PhotosGallery, Button, Loader } from 'components';
 import { useEffect, useState } from 'react';
 
 export const Photos = () => {
@@ -9,30 +9,47 @@ export const Photos = () => {
 
   const [error, setError] = useState('');
 
+  const [page, setPage] = useState(1);
+
+  const [isLoading, setIsLoading] = useState(false);
+
   const onSubmitHandler = value => {
     setSearchValue(value);
+  };
+
+  const handlerLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
   useEffect(() => {
     if (!searchValue) return;
 
     const fetch = async () => {
+      setIsLoading(true);
       try {
-        const data = await getPhotos(searchValue);
-        setPhotos(data.photos);
+        const data = await getPhotos(searchValue, page);
+        setPhotos(prevPhoto => {
+          return [...prevPhoto, ...data.photos];
+        });
       } catch (error) {
         setError(error.message);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetch();
-  }, [searchValue]);
+  }, [searchValue, page]);
 
   return (
     <>
+      {isLoading && <Loader />}
       <Text textAlign="center">Let`s begin search 🔎</Text>
 
       <Form onSubmit={onSubmitHandler} />
+      {error && <Text textAlign="center">{error}</Text>}
+      <PhotosGallery photos={photos} />
+      <Button onClick={handlerLoadMore}>Load more</Button>
     </>
   );
 };
